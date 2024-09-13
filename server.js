0e692edf66d8b471a6327449734b46745753b822
Debarghya Mondal <debarghya888@gmail.com>
2024-09-14 02:09:21 +0530

    kkll

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+
+app.use(express.static(path.join(__dirname, 'database')))
+
 app.get('/', (req, res) => {
     res.send('Hello World! from express test app for learnemy')
 })
